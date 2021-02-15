596f0a4d61a5c409e1f3ca027b7d1d01bfa36abd
Michael Olatunji <tundexmike@gmail.com>
2021-02-15 01:54:58 +0100

    get array size method, test array in main

--- src/CustomArray/HighArray.java
+++ src/CustomArray/HighArray.java
@@ -10,19 +10,22 @@ public class HighArray {
     }
 
     public boolean find(long item){
-        for(int i = 0; i < this.nItems+1; i++){
+        for(int i = 0; i < this.nItems; i++){
             if(arr[i] == item){
                 return true;
             }
         }
-        return false;
+        return arr[nItems] == item;
     }
 
     public int findIndex(long item){
-        for(int i = 0; i < this.nItems+1; i++){
+        for(int i = 0; i < this.nItems; i++){
             if(arr[i] == item){
                 return i;
             }
+        }
+        if(arr[nItems] == item){
+            return nItems;
         }
         return -1;
     }
@@ -36,18 +39,22 @@ public class HighArray {
         int index;
         if(this.findIndex(item) >= 0){
             index = this.findIndex(item);
+            arr[index] = arr[index + 1];
+            nItems--;
+            return true;
         } else{
             return false;
         }
-        arr[index] = arr[index + 1];
-        nItems--;
-        return true;
     }
 
     public void display(){
-        for(int i = 0; i < nItems+1; i++){
+        for(int i = 0; i < nItems; i++){
             System.out.print(arr[i] +" ");
         }
         System.out.println("");
+    }
+
+    public int size(){
+        return nItems;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,31 @@
+import CustomArray.HighArray;
+
 import java.time.LocalDate;
 
 public class Main {
     public static void main(String args[]){
-//        System.out.println("Data Structures");
-//        boolean a = isSpecial("abbbba");
 
+        int max = 100;
+        HighArray arr = new HighArray(max);
+        arr.insert(77); // insert 10 items
+        arr.insert(99);
+        arr.insert(44);
+        arr.insert(55);
+        arr.insert(22);
+        arr.insert(88);
+        arr.insert(11);
+        arr.insert(00);
+        arr.insert(66);
+        arr.insert(33);
+        arr.display();
 
+        int searchKey = 35;
+        if( arr.find(searchKey)) System.out.println("Found " + searchKey);
+        else System.out.println("Can’t find " + searchKey);
+        arr.delete(00);
+        arr.delete(55);
+        arr.delete(99);
+        arr.display();
 
     }
-
-//    public static boolean isSpecial(String text){
-//        String tmp = alterText(text);
-//        return text.equals(tmp);
-//    }
-//
-//    public static String alterText(String text){
-//        return text.charAt(text.length() - 1)+alterText(text.substring(0, text.length()-2));
-//    }
 }
