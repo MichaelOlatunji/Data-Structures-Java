0a9f56c129cb1c07082b46cc8b3d89eb8e7b648d
Michael Olatunji <tundexmike@gmail.com>
2021-02-18 23:36:50 +0100

    implementing binary search for find and findIndex method in OrderedArray

--- src/CustomArray/OrderedArray.java
+++ src/CustomArray/OrderedArray.java
@@ -9,6 +9,43 @@ public class OrderedArray extends HighArray{
     // TODO: Implement a Binary Search for find method
 
     @Override
+    public boolean find(long key){
+        int first = 0;
+        int last = this.arr.length + 1;
+        int mid = (first + last)/2;
+        while(first <= last){
+            if(this.arr[mid] == key){
+                return true;
+            } else if(this.arr[mid] < key){
+                first = mid + 1;
+            } else {
+                last = mid - 1;
+            }
+            mid = (first + last)/2;
+        }
+
+        return false;
+    }
+
+    @Override
+    public int findIndex(long key){
+        int first = 0;
+        int last = this.arr.length + 1;
+        int mid = (first + last)/2;
+        while(first <= last){
+            if(this.arr[mid] == key){
+                return mid;
+            } else if(this.arr[mid] < key){
+                first = mid + 1;
+            } else {
+                last = mid - 1;
+            }
+            mid = (first + last)/2;
+        }
+        return -1;
+    }
+
+    @Override
     public void insert(long item){
         int i;
         for(i = 0; i < this.nItems; i++){
